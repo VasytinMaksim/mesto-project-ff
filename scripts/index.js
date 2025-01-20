74dc2977669a6fe2a97ad1b61ec9536f3d51e482
Vsytin Maksim <vasytinmaksim22@yandex.ru>
2025-01-20 20:28:31 +0300

    Исправил ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,11 @@ const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function cardCreate(card) {
+function cardCreate(card, deleteCard) {
   const cardElement = cards.querySelector('.places__item').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   cardImage.src = card.link;
+  cardImage.alt = card.alt;
   cardElement.querySelector('.card__title').textContent = card.name;
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', () => deleteCard(cardElement));
@@ -31,7 +32,7 @@ function addCard(cardElement, cardContainer) {
 }
 
 initialCards.forEach((card) => {
-    const cardCreated = cardCreate(card);
+    const cardCreated = cardCreate(card, deleteCard);
     addCard(cardCreated, cardContainer);
 });
 
