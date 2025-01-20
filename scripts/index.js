f16838aec26771f449347d580faa825daa63e7de
Vsytin Maksim <vasytinmaksim22@yandex.ru>
2025-01-20 16:25:08 +0300

    Первая отправка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
 
+const cards = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function cardCreate(card) {
+  const cardElement = cards.querySelector('.places__item').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardDelete) {
+  cardDelete.remove(); 
+}
+
 // @todo: Вывести карточки на страницу
+
+function addCard(cardElement, cardContainer) {
+  cardContainer.append(cardElement);
+}
+
+initialCards.forEach((card) => {
+    const cardCreated = cardCreate(card);
+    addCard(cardCreated, cardContainer);
+});
+
